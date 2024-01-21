38a6faf66415f7ff0a28f2c92859bd45cbfdc4e6
swadebsharma6 <swadebriva2012@gmail.com>
2024-01-22 01:41:48 +0600

    added navbar with mui

---
+++ src/components/shared/Navbar.js
@@ -0,0 +1,85 @@
+'use client'
+import logo from '@/assets/newslogo.png';
+import { Button, IconButton, Stack } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Image from 'next/image';
+
+// Icons comes Here
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import Link from 'next/link';
+
+const navItems = [
+    {
+        route: 'Home',
+        pathname: '/'
+    },
+    {
+        route: 'Pages',
+        pathname: '/pages'
+    },
+    {
+        route: 'Category',
+        pathname: '/category'
+    },
+    {
+        route: 'News',
+        pathname: '/news'
+    },
+    {
+        route: 'Contact',
+        pathname: '/contact'
+    },
+    {
+        route: 'About',
+        pathname: '/about'
+    },
+
+];
+
+
+const Navbar = () => {
+
+ 
+  return (
+    <AppBar position="static" className='bg-black'>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+
+       <Image src={logo} alt='Logo' width={100} height={100}></Image>
+
+          <Box className="w-full  text-center">
+            {navItems.map((item, idx) => (
+              <Link key={idx} href={item.pathname}>
+                <Button className='text-white'>{item.route}</Button>
+              </Link>
+            ))}
+          </Box>
+
+          <Box>
+                <Stack direction="row" spacing={2} sx={{
+                    "& svg":{
+                        color: 'orangered'
+                    }
+                }}>
+                <IconButton>
+                <FacebookIcon/>    
+              </IconButton>
+              <IconButton>
+               <TwitterIcon/>   
+              </IconButton>
+              <IconButton>
+                <GitHubIcon/>   
+              </IconButton>
+                </Stack>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Navbar;
